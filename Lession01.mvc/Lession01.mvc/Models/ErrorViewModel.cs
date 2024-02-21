05943574a7230ee6f068a6f81664dc21bf073c2c
hoa <lymaihoa28@gmail.com>
2024-02-21 20:51:02 +0700

    hoan thanh

---
+++ Lession01.mvc/Lession01.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lession01.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
